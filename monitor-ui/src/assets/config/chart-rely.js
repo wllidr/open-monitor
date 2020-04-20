43e31729c545cfb931b115907db88ae37d3dbfc4
pobu168 <244202969@qq.com>
2020-04-20 09:37:33 +0800

    Plugin test

--- monitor-ui/src/assets/config/chart-rely.js
+++ monitor-ui/src/assets/config/chart-rely.js
@@ -144,78 +144,78 @@ export const drawChart = function(that,config,userConfig) {
       bottom: '40' ,
       containLabel: true
     },
-    xAxis: {
-      type: 'time',
-      axisLabel: {
-        textStyle: {
-          color: chartTextColor
-        },
-        formatter: function (value) {
-          return echarts.format.formatTime('MM-dd\nhh:mm:ss', value)
-        }
-      },
-      boundaryGap : false,
-      axisLine:{
-        lineStyle:{
-          color:'#a1a1a2'
-        }
-      }, 
-      splitLine: {
-        show: true,
-        lineStyle:{
-          color: ['#a1a1a2'],
-          width: 1,
-          type: 'solid'
-        }
-      }
-    },
-    yAxis: [
-      {
-        type: 'value',
-        axisLabel: {
-          textStyle: {
-            color: chartTextColor
-          },
-          show: true,
-          interval: 'auto',
-          formatter: (value) => {
-            let unit = ''
-            if (value > 1024*1024*1024*1024) {
-              value = value / (1024*1024*1024*1024)  
-              unit = 'T'
-            } else if (value > 1024*1024*1024) {
-              value = value / (1024*1024*1024)  
-              unit = 'G'
-            } else if (value > 1024*1024) {
-              value = value / (1024*1024)  
-              unit = 'M'
-            } else if (value > 1024) {
-              value = value / 1024  
-              unit = 'K'
-            } else {
-              return value + ' ' + config.yaxis.unit
-            }
-            let newValue = Number.isInteger(value) ? value : value.toFixed(3)
-            return newValue + ' ' + unit + config.yaxis.unit
-          }
-        },
-        show: true,
-        axisLine:{
-          lineStyle:{
-            color:'#a1a1a2'
-          }
-        }, 
-        splitLine: {
-          show: true,
-          lineStyle:{
-            color: ['#a1a1a2'],
-            width: 1,
-           type: 'solid'
-          }
-        }
-      },
-    ],
-    // series: config.series
+    // xAxis: {
+    //   type: 'time',
+    //   axisLabel: {
+    //     textStyle: {
+    //       color: chartTextColor
+    //     },
+    //     formatter: function (value) {
+    //       return echarts.format.formatTime('MM-dd\nhh:mm:ss', value)
+    //     }
+    //   },
+    //   boundaryGap : false,
+    //   axisLine:{
+    //     lineStyle:{
+    //       color:'#a1a1a2'
+    //     }
+    //   }, 
+    //   splitLine: {
+    //     show: true,
+    //     lineStyle:{
+    //       color: ['#a1a1a2'],
+    //       width: 1,
+    //       type: 'solid'
+    //     }
+    //   }
+    // },
+    // yAxis: [
+    //   {
+    //     type: 'value',
+    //     axisLabel: {
+    //       textStyle: {
+    //         color: chartTextColor
+    //       },
+    //       show: true,
+    //       interval: 'auto',
+    //       formatter: (value) => {
+    //         let unit = ''
+    //         if (value > 1024*1024*1024*1024) {
+    //           value = value / (1024*1024*1024*1024)  
+    //           unit = 'T'
+    //         } else if (value > 1024*1024*1024) {
+    //           value = value / (1024*1024*1024)  
+    //           unit = 'G'
+    //         } else if (value > 1024*1024) {
+    //           value = value / (1024*1024)  
+    //           unit = 'M'
+    //         } else if (value > 1024) {
+    //           value = value / 1024  
+    //           unit = 'K'
+    //         } else {
+    //           return value + ' ' + config.yaxis.unit
+    //         }
+    //         let newValue = Number.isInteger(value) ? value : value.toFixed(3)
+    //         return newValue + ' ' + unit + config.yaxis.unit
+    //       }
+    //     },
+    //     show: true,
+    //     axisLine:{
+    //       lineStyle:{
+    //         color:'#a1a1a2'
+    //       }
+    //     }, 
+    //     splitLine: {
+    //       show: true,
+    //       lineStyle:{
+    //         color: ['#a1a1a2'],
+    //         width: 1,
+    //        type: 'solid'
+    //       }
+    //     }
+    //   },
+    // ],
+    series: config.series // 此处没问题，检查之上代码
   }
 
   if (finalConfig.title) {
